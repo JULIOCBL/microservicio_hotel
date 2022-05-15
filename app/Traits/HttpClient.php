d60f72620a51eaf1fa5245cab59a496b715a7cf4
JULIOCBL <60810419+JULIOCBL@users.noreply.github.com>
2022-05-15 16:53:02 -0500

    UPDATE

--- app/Traits/HttpClient.php
+++ app/Traits/HttpClient.php
@@ -7,38 +7,7 @@ trait HttpClient
 
     public function performRequest($method, $requestUrl,  $formParams = [],  $headers = [])
     {
-        /*   $headers = array();
-        $headers[] = 'Accept: application/json';
-
-
-        if (isset($this->secret)) {
-            $headers[] = "Authorization: {$this->secret}";
-        }
-
-        $ch = curl_init();
-
-        if ($method == 'POST') {
-            $headers[] = 'Content-Type: application/json';
-            curl_setopt($ch, CURLOPT_URL,  $this->baseUri . $requestUrl);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POST, $method);
-            
-            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-            $response = curl_exec($ch);
-        } else if ($method == 'GET') {
-            curl_setopt($ch, CURLOPT_URL, $this->baseUri . $requestUrl . $this->url($formParams));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST,  $method);
-            curl_setopt($ch, CURLOPT_HEADER,  $headers);
-            $response = curl_exec($ch);
-        }
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
-        }
-        curl_close($ch);
- */
-
-        $headers = array();
+       /*  $headers = array();
         $headers[] = 'Accept: application/json';
         $headers[] = "Authorization: {$this->secret}";
 
@@ -62,8 +31,9 @@ trait HttpClient
         if (curl_errno($ch)) {
             echo 'Error:' . curl_error($ch);
         }
-        curl_close($ch);
-        return $response;
+
+        curl_close($ch); */
+        return ['URL' =>  $this->baseUri . $requestUrl . $this->url($formParams)];
     }
 
 
