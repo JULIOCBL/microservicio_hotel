b462538e99ba917522aaad4496062010ac839035
JULIOCBL <60810419+JULIOCBL@users.noreply.github.com>
2022-05-15 16:49:57 -0500

    UPDATE

--- app/Schemas/GeoAutocomplete/GeoAutocompleteV2RQ.php
+++ app/Schemas/GeoAutocomplete/GeoAutocompleteV2RQ.php
@@ -22,6 +22,6 @@ class GeoAutocompleteV2RQ
         $this->baseUri =  config('links.sabre.base_uri');
         $this->secret =  config('links.sabre.token');
 
-        return  $request /*  $this->performRequest('GET', '/v2/geo/autocomplete', $request) */ ;
+        return  ['SDCS' =>config('links.sabre.base_uri'), 'SDCSSD' =>   config('links.sabre.token')] /*  $this->performRequest('GET', '/v2/geo/autocomplete', $request) */ ;
     }
 }
